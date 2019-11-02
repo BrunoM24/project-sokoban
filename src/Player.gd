7c1f32e9c319371e8e08dec32992c71d92643a1f
BrunoM24 <brunocostamatos.24@gmail.com>
2019-11-02 15:06:31 +0000

    Updated the Player movement logic

--- src/Player.gd
+++ src/Player.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-var speed := 64
+var speed := 128
 var targetPosition := Vector2()
 var velocity := Vector2()
 
@@ -11,26 +11,49 @@ func _ready() -> void:
 	targetPosition = position
 
 
-func _process(delta : float) -> void:
+func _physics_process(delta : float) -> void:
+	#var t : = Vector2()
 	
-	velocity = (targetPosition - position).normalized() * speed
+	#velocity = (targetPosition - position).normalized() * speed
 	
-	if velocity.length() > 0:
-		playAnimation(velocity.normalized())
+	#if velocity.length() > 0:
+	#	playAnimation(velocity.normalized())
+	#else:
+	#	animationPlayer.stop()
+	
+	#if(position.distance_to(targetPosition) < speed * delta):
+	#	position = targetPosition
+	#else:
+	#	position += velocity * delta 
+	
+	#print(targetPosition)
+	
+	#move_and_slide(targetPosition.normalized() * speed, Vector2())
+	
+	#var motion : = Vector2()
+	#motion.x = int(Input.get_action_strength("ui_right")) - int(Input.get_action_strength("ui_left"))
+	#motion.y = int(Input.get_action_strength("ui_down")) - int(Input.get_action_strength("ui_up"))
+	
+	#print((targetPosition - position).normalized())
+	
+	var moveTo := Vector2()
+	
+	if(position.distance_to(targetPosition) > 0.5):
+		moveTo = (targetPosition - position).normalized()
+	
+	if(moveTo != Vector2.ZERO):
+		playAnimation(moveTo)
 	else:
 		animationPlayer.stop()
 	
-	if(position.distance_to(targetPosition) < speed * delta):
-		position = targetPosition
-	else:
-		position += velocity * delta 
+	move_and_slide(moveTo * speed)
 
 
 func _unhandled_input(event : InputEvent) -> void:
 	
 	var direction : Vector2
 	
-	if targetPosition != position:
+	if position.distance_to(targetPosition) > 4:
 		return
 	
 	if event.is_action_pressed("ui_up"):
@@ -44,7 +67,8 @@ func _unhandled_input(event : InputEvent) -> void:
 	else:
 		direction = Vector2.ZERO
 	
-	targetPosition = position + direction * speed
+	if !test_move(transform, direction * 64):
+		targetPosition = position + direction * speed
 
 
 func playAnimation(direction : Vector2) -> void:
