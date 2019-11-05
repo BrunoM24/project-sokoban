c8189035a653a8c4ca040c0d6d87d7f198603c52
BrunoM24 <brunocostamatos.24@gmail.com>
2019-11-05 11:46:55 +0000

    Added Floor TextureRect to the World Scene

--- src/World.gd
+++ src/World.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
 onready var wonPop : PopupMenu = $CanvasLayer/WinPopup
+onready var floorTile : TextureRect = $Floor
 
-func _on_Platform_gameWon():
+
+func _ready() -> void:
+	floorTile.rect_size = get_viewport_rect().size
+
+
+func _on_Platform_gameWon() -> void:
 	wonPop.show()
